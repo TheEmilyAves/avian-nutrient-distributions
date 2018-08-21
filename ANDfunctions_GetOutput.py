d1b9bc3c4473f98770c0f54ad76f729054834123
Emily Webb <Emily@emilys-air-9.mobile.asu.edu>
2018-08-21 06:35:43 -0700

    consolidate versions of getOutput functions to one getCol function

--- ANDfunctions_GetOutput.py
+++ ANDfunctions_GetOutput.py
@@ -5,58 +5,33 @@ Functions used to generate output files for the Avian Nutrient Distributions Pro
 # import modules
 import csv
 
-
-def getOutput_rp(bird):
+def getCol(bird):
     col_birdid = []
     col_sex = []
     col_treatment = []
     col_tissuetype = []
-    col_rp = []
-    for bird_id,bird_obj in bird.items():
-        for tissue_type,tissue_obj in bird_obj.tissues.items():
-            col_birdid.append(bird_id)
-            col_sex.append(bird_obj.sex)
-            col_treatment.append(bird_obj.treatment)
-            col_tissuetype.append(tissue_type)
-            col_rp.append(bird_obj.relative_p[tissue_type])
-    return col_birdid, col_sex, col_treatment, col_tissuetype, col_rp
-
-
-def getOutput_p(bird):
-    col_birdid = []
-    col_sex = []
-    col_treatment = []
-    col_tissuetype = []
-    col_tip = []
     col_mt = []
-    for bird_id,bird_obj in bird.items():
-        for tissue_type,tissue_obj in bird_obj.tissues.items():
-            col_birdid.append(bird_id)
-            col_sex.append(bird_obj.sex)
-            col_treatment.append(bird_obj.treatment)
-            col_tissuetype.append(tissue_type)
-            col_tip.append(bird_obj.tissue_p[tissue_type])
-            col_mt.append(tissue_obj.mass_total)
-    return col_birdid, col_sex, col_treatment, col_tissuetype, col_tip, col_mt
-
-
-def getOutput_conc(bird):
-    col_birdid = []
-    col_sex = []
-    col_treatment = []
-    col_tissuetype = []
+    col_tip = []
+    col_rp = []
     col_conc = []
+    col_tc = []
+    col_tbc = []
     for bird_id,bird_obj in bird.items():
         for tissue_type,tissue_obj in bird_obj.tissues.items():
             col_birdid.append(bird_id)
             col_sex.append(bird_obj.sex)
             col_treatment.append(bird_obj.treatment)
             col_tissuetype.append(tissue_type)
+            col_mt.append(tissue_obj.mass_total)
+            col_tip.append(bird_obj.tissue_p[tissue_type])
+            col_rp.append(bird_obj.relative_p[tissue_type])
             col_conc.append(bird_obj.carot_conc[tissue_type])
-    return col_birdid, col_sex, col_treatment, col_tissuetype, col_conc
+            col_tc.append(tissue_obj.total_carot)
+            col_tbc.append(bird_obj.totalbodycarot)
+    return col_birdid, col_sex, col_treatment, col_tissuetype, col_mt, col_tip, col_rp, col_conc, col_tc, col_tbc
 
 
-def colToRow(col0, col1, col2, col3, col4):
+def colToRow(col0, col1, col2, col3, col4, col5, col6, col7, col8, col9):
     list_rows = []
     c = 0
     for i in col0:
@@ -66,6 +41,11 @@ def colToRow(col0, col1, col2, col3, col4):
         row.append(col2[c])
         row.append(col3[c])
         row.append(col4[c])
+        row.append(col5[c])
+        row.append(col6[c])
+        row.append(col7[c])
+        row.append(col8[c])
+        row.append(col9[c])
         list_rows.append(row)
         c += 1
     return list_rows
